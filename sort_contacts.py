ac6456ef9cd654872063c1993a3eb5db044e4132
mherna42 <49410553+mherna42@users.noreply.github.com>
2019-05-09 17:58:19 -0600

    Update sort_contacts.py

--- sort_contacts.py
+++ sort_contacts.py
@@ -1,3 +1,7 @@
-def sort_contacts(param):
-    # code here
-    return ""
+def sort_contacts(contacts):
+    contacts_list = list(contacts.items())
+    contacts_list.sort()
+    return_list = []
+    for (k, v) in contacts_list:        
+        return_list.append((k,)+v)
+    return return_list
